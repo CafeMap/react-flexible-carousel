0fff978f2c983dc5a816ec85b51db62fdc3c644a
Whien <sal95610@gmail.com>
2016-11-16 11:30:48 +0800

    add webpack production builder

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack')
+
+module.exports = {
+  output: {
+    library: 'ReactFlexibleCarousel',
+    libraryTarget: 'umd'
+  },
+  externals: [
+    {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react'
+      }
+    }
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  },
+  node: {
+    Buffer: false
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
+}
